b624cc3f1c280585ff34d87ffe4e0759d266f35c
Yuji Nakamura <zuzukikanyada@gmail.com>
2021-01-25 17:50:57 +0900

    6_c解答を追加

---
+++ ITP1/6_c.py
@@ -0,0 +1,15 @@
+# n = int(input())
+n = 1
+
+arr = [[[0 for i in range(10)] for j in range(3)] for k in range(4)]
+
+for t in range(n):
+    # b,f,r,v = map(int,input().split())
+    b,f,r,v = [1,1,3,8]
+    arr[b-1][f-1][r-1] += v
+
+for i in range(4):
+    if i != 0:
+        print("#"*20)
+    for j in range(3):
+        print(*[""]+arr[i][j])
